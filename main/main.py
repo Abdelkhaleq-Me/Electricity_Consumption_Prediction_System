8fc5d6732ec5e0fc7246dc83b2c05a4161034f31
Abdelkhaleq-Me <a.mehdaoui@hns-re2sd.dz>
2023-12-26 01:50:03 +0100

    added a main folder containsthe main code and the data

---
+++ main/main.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+data = pd.read_csv("Electricity_consumption_history.csv")
